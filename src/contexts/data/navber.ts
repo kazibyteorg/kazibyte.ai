6dfb48727060b6dc4f712169b30a453094939b9a
zobkazi <zobaidulkazi64.official@gmail.com>
2024-07-12 11:14:39 +0600

    feat: types init zod used

--- src/contexts/data/navber.ts
+++ src/contexts/data/navber.ts
@@ -1,12 +1,40 @@
 import Home from "@/assets/icons/approval-svgrepo-com.svg"
+import {z} from "zod"
+
+const HomeSchema = z.object({
+    title: z.string(),
+    href: z.string(),
+    icon: z.string(),
+    Home,
+})
 
 
-const navLink = [
-    { title: "Features", href: "#features", icon: "Home", Home },
-    { title: "Solutions", href: "#solutions" },
-    { title: "Resources", href: "#resources" },
-    { title: "Pricing", href: "#pricing" },
-]
-
+const navLink: z.infer<typeof HomeSchema>[] = [
+    {
+        title: "Home",
+        href: "/",
+        icon: Home
+    },
+    {
+        title: "About",
+        href: "/about",
+        icon: Home
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+        icon: Home
+    },
+    {
+        title: "Blog",
+        href: "/blog",
+        icon: Home
+    },
+    {
+        title: "Services",
+        href: "/services",
+        icon: Home
+    },
+] 
 
 export default navLink;
